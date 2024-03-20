821b520d536ba57c6128d0178aae2af1262d4ea9
MathysM31 <mathys.marcia31@gmail.com>
2024-03-20 12:05:42 +0100

    Finir 2-6 corriger isVendeur

--- src/frontiere/BoundaryLibererEtal.java
+++ src/frontiere/BoundaryLibererEtal.java
@@ -1,6 +1,7 @@
 package frontiere;
 
 import controleur.ControlLibererEtal;
+import villagegaulois.Etal;
 
 public class BoundaryLibererEtal {
 	private ControlLibererEtal controlLibererEtal;
@@ -10,7 +11,19 @@ public class BoundaryLibererEtal {
 	}
 
 	public void libererEtal(String nomVendeur) {
-		//TODO a completer
+		boolean vendeurReconnu = controlLibererEtal.isVendeur(nomVendeur);
+		if (!vendeurReconnu) {
+			System.out.println("Mais vous n'êtes pas inscrit sur notre marché aujourd'hui !");
+		} else {
+			String donneesEtal[] =  controlLibererEtal.libererEtal(nomVendeur);
+			if (donneesEtal[0].equals("true")) {
+				String produit = donneesEtal[2];
+				String quantiteInitial = donneesEtal[3];
+				String quantiteVendu = donneesEtal[4];
+				System.out.println("Vous avez vendu "+ quantiteVendu +" sur "+ quantiteInitial +" "+ produit +".");
+				System.out.println("Au revoir "+ nomVendeur +", passez une bonne journée.");
+			}
+		}
 	}
 
 }
